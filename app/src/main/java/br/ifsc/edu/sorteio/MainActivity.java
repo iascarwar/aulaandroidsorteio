1c32e055e38c9bdc0e868eed0061e398e9a242b5
Axel <axelgeorg16@gmail.com>
2020-02-27 11:26:08 -0300

    sortear certo

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    EditText editTextinicial, editTextfinal;
+    EditText editTextinicial, editTextfinal; // esta aqui em cima para que possa ser usado pelo codigo todo
     TextView textViewResultado;
 
     @Override
@@ -16,16 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextinicial = findViewById(R.id.editTextinicial);
+        editTextinicial = findViewById(R.id.editTextinicial); // R = contem todos os id's que tem dentro do codigo
         editTextfinal = findViewById(R.id.editTextfinal);
         textViewResultado = findViewById(R.id.textViewResultado);
     }
 
-    public void sorteia(View view) {
-       int inicial = Integer.parseInt(editTextinicial.getText().toString());
-       int fim = Integer.parseInt(editTextfinal.getText().toString());
+    public void sorteia(View view) { // isso aqui é o botão (View view)
+        Button but = (Button) view;
+        but.setText("Sorteado");
 
-       textViewResultado.setText(Integer.toString((inicial)));
+        int inicial = Integer.parseInt(editTextinicial.getText().toString());
+        int fim = Integer.parseInt(editTextfinal.getText().toString());
 
+        Random rand = new Random();
+        int numsorteado = rand.nextInt((fim - inicial) + 1) + inicial;
+
+        textViewResultado.setText(Integer.toString((numsorteado)));
+
+
+        }
     }
-}
